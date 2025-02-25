c926458e9f867422e7df872e852d2338672022d8
Bryan Arellano <temporal@sasf.local>
2025-02-25 14:46:00 -0500

    Verificacion

--- practicaBryan/src/main/java/com/poryect1/practicaBryan/Modelo/Pedido.java
+++ practicaBryan/src/main/java/com/poryect1/practicaBryan/Modelo/Pedido.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +20,7 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
+    @NotBlank
     private long id;
 
     @Column
