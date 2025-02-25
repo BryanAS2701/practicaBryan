0970ea9c8287f8429e2b28a3fcc0e76dee968b83
Bryan Arellano <temporal@sasf.local>
2025-02-25 14:37:27 -0500

    otro commit

--- practicaBryan/src/main/java/com/poryect1/practicaBryan/Repository/Pedido_Repository.java
+++ practicaBryan/src/main/java/com/poryect1/practicaBryan/Repository/Pedido_Repository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.poryect1.practicaBryan.Modelo.Pedido;
 
 public interface Pedido_Repository extends JpaRepository<Pedido, Long> {
-
 }
